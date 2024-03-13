a5fc48e71d7adc2e2e8b94b2c12c0839a71e6e54
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-13 18:58:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s.length === 0 || s.length === 0) {
+        return ''; // If either string is empty, return an empty string
+    }
+    
+    return s[0].toUpperCase() + s[0].toLowerCase(); // Return the initial letters of the strings
 }
+
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
